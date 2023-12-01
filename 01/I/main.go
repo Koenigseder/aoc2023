5a0c93dda1d94f937c70acd724acd678a7e1d690
Koenigseder <kevin.koenigseder@outlook.com>
2023-12-01 13:28:31 +0100

    Day 1 - Part I

---
+++ 01/I/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	fmt.Println("Day 1 2023 - Part I\n")
+
+	// Read input file
+	file, err := os.Open("./01/I/input.txt")
+	if err != nil {
+		log.Fatalf("Error while reading file: %v", err)
+	}
+	defer file.Close()
+
+	sum := 0 // Final sum of all numbers
+
+	// Read file line by line
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lineText := scanner.Text()
+		firstInt := -1
+		lastInt := -1
+
+		// Loop over each and every char in a line and check if it is a number
+		for _, char := range lineText {
+			if num, err := strconv.Atoi(fmt.Sprintf("%c", char)); err == nil {
+				// Check if firstInt is already filled with a valid value
+				if firstInt == -1 {
+					firstInt = num
+					continue
+				}
+
+				// Set lastInt to the next number's value - in the end you have the last number's value
+				lastInt = num
+			}
+		}
+
+		// In case there is only one number, set the lastInt to the value of firstInt's
+		if lastInt == -1 {
+			lastInt = firstInt
+		}
+
+		// Add value of combined numbers to sum
+		sum += firstInt*10 + lastInt
+	}
+
+	// Check for errors during scanning the file
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("Error scanning file: %v", err)
+	}
+
+	// Print result
+	fmt.Printf("The overall sum is: %d\n", sum)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Koenigseder/aoc2023
+
+go 1.20
